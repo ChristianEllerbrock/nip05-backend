c0bf2aafcc587cbe8ea6a2e9d2460354e1d149d8
Christian Ellerbrock <christian.ellerbrock@accenture.com>
2023-01-26 18:34:25 +0100

    2023-01-26 (2)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import "reflect-metadata";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { EnvService } from "./services/env-service";
+
+
+// Load any environmental variables from the local .env file
+dotenv.config();
+
+
+const app: Express = express();
+const port = EnvService.instance.env.PORT;
+
 console.log("Hallo");
---
+++ src/services/env-service.ts
@@ -0,0 +1,68 @@
+/* eslint-disable no-console */
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export class EnvServiceEnv {
+    PORT!: string;
+
+    NODE_TLS_REJECT_UNAUTHORIZED!: string;
+
+    DATABASE_URL!: string;
+    SHADOW_DATABASE_URL!: string;
+}
+
+export class EnvService {
+    // #region Singleton
+
+    private static _instance: EnvService;
+    static get instance() {
+        if (this._instance) {
+            return this._instance;
+        }
+
+        this._instance = new EnvService();
+        return this._instance;
+    }
+
+    // #endregion Singleton
+
+    constructor() {
+        this._buildEnv();
+    }
+
+    // #region Public Properties
+
+    get env() {
+        return this._env;
+    }
+
+    // #endregion Public Properties
+
+    // #region Private Properties
+
+    private _env!: EnvServiceEnv;
+
+    // #endregion Private Properties
+
+    // #region Private Methods
+
+    private _buildEnv() {
+        const env = new EnvServiceEnv();
+
+        console.log('Trying to read the following keys from ENV:');
+
+        Object.keys(env).forEach(key => console.log(' ' + key));
+
+        Object.keys(env).forEach(key => {
+            if (typeof process.env[key] === 'undefined') {
+                throw new Error(`Could not read required ENV key '${key}' from ENV.`);
+            }
+
+            (env as any)[key] = process.env[key];
+        });
+        this._env = env;
+    }
+
+    // #endregion Private Methods
+}
